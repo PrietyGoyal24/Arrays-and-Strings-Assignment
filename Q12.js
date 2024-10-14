68cd4858def4b5e555ef1346cbb01548f281e8c1
Priety Goyal <priety2023@gmail.com>
2024-10-15 00:41:49 +0530

    Q-12 done

---
+++ Q12.js
@@ -0,0 +1,9 @@
+
+
+let toDoList = ["Buy groceries", "Complete project", "Exercise", "Read a book", "Call a friend"];
+
+toDoList.unshift("Check emails");
+
+toDoList.pop();
+
+console.log("Updated to-do list: ", toDoList);
